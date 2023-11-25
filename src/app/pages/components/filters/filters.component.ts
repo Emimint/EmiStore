0fe43a7c27d737045a4c7fd91171c5401aeab26f
Emimint <90863470+Emimint@users.noreply.github.com>
2023-11-25 15:47:15 -0500

    add dynamic implementation of product categories

--- src/app/pages/components/filters/filters.component.ts
+++ src/app/pages/components/filters/filters.component.ts
@@ -1,12 +1,33 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { StoreService } from '../../../services/store.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html'
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() showCategory = new EventEmitter<string>();
-  categories = ["Shoes", "Sports"];
+  categoriesSubscription: Subscription | undefined;
+  categories: Array<string> | undefined;
+
+  constructor(private storeService: StoreService){}
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  ngOnDestroy(): void {
+    if(this.categoriesSubscription)
+      this.categoriesSubscription?.unsubscribe();
+  }
+
+  getAllCategories(): void{
+    this.categoriesSubscription = this.storeService.getAllCategories()
+  .subscribe(_categories => {
+    this.categories = _categories;
+  });
+  }
+
 
   onShowCategory(category: string): void{
     this.showCategory.emit(category);
